f5bea84963f23e3513a18c8046523646f7de5e25
Raj_devraj <40891170+Rajdevraj@users.noreply.github.com>
2021-11-26 11:37:05 +0530

    Create Remove.js

---
+++ Remove.js
@@ -0,0 +1,21 @@
+function convertToStr(input){
+  if (typeof input === 'object' && input !== null){
+    input = JSON.stringify(input);
+    return input
+}else{
+    return input
+}
+}
+
+function removeBrakets(inputStr){
+  inputStr = convertToStr(inputStr);
+  inputStr = inputStr.toLowerCase();
+  inputStr = inputStr.replace(/[\[\]']+/g,'');
+  return inputStr
+}
+
+str = ["example@dev.com"];
+str1 = '["example@dev.com"]'
+console.log(removeBrakets(str))
+console.log(removeBrakets(str1))
+
